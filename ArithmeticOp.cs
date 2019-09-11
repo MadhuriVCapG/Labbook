5db5cee6a714744f6c7c78e1f9775d8e43f2cc98
MadhuriVCapG <54537293+MadhuriVCapG@users.noreply.github.com>
2019-09-11 11:17:18 +0530

    Add files via upload

---
+++ ArithmeticOp.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ArithmeticOp
+{
+    public class ArithmeticOperations
+    {
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+        public int Divide(int a, int b)
+        {
+            return a / b;
+        }
+
+        public double Divide(double a, double b)
+        {
+            return a / b;
+        }
+
+        public int Modulus(int a, int b)
+        {
+            return a % b;
+        }
+    }
+}
